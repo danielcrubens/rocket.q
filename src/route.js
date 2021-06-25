a145b3939ffc224ea557790120d82bb1dec9b7d0
danielrubens <dcr.tecnologi@gmail.com>
2021-06-24 22:54:06 -0300

    persistindo os dados :rocket:

---
+++ src/route.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const route = express.Router()
+
+route.get('/',(req, res) => res.render("index"))
+route.get('/room',(req,res)=>res.render("room"))
+route.get('/create-pass',(req,res)=>res.render("create-pass"))
+// formato que o formulario de dentro da modal tem que passar a info
+/* route.post('/room/:room/:question/:action') */
+module.exports = route
